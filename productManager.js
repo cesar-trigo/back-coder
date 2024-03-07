497c1301f03cb3ca1a60cc6a5044a5580bcde5bf
Cesar Trigo <cesar.luis-trigo@hotmail.com>
2024-03-06 21:17:23 -0300

    The Product Manager class and its methods were optimized.

--- productManager.js
+++ productManager.js
@@ -3,42 +3,25 @@ class ProductManager {
   #products;
   static ID_AUTOINCREMENTABLE = 1;
 
-
   constructor() {
     this.#products = [];
   };
 
 
   addProduct = (title, description, price, thumbnail, code, stock) => {
+    const codeValidator = this.#products.some(e => e.code === code);
 
-    const validadorDeCode = this.#products.some(e => e.code === code);
-
-    if (!title || !description || !price || !thumbnail || !code || !stock || validadorDeCode) {
-      return console.log(`“Not found”`)
-    } else {
-      this.#products.push({
-        id: ProductManager.ID_AUTOINCREMENTABLE++,
-        title: title,
-        description: description,
-        price: price,
-        thumbnail: thumbnail,
-        code: code,
-        stock: stock,
-      })
-    }
+    !title || !description || !price || !thumbnail || !code || !stock || codeValidator ? console.log(`Not found`)
+      : this.#products.push({ id: ProductManager.ID_AUTOINCREMENTABLE++, title, description, price, thumbnail, code, stock, })
   };
+
 
   getProducts = () => console.log(this.#products)
 
+
   getProductById = (prodId) => {
-    const productoPorId = this.#products.find(e => e.id === prodId)
-
-
-    if (productoPorId) {
-      return console.log(productoPorId)
-    } else {
-      return console.log(`“Not found”`)
-    };
+    const productoPorId = this.#products.find(e => e.id === prodId);
+    productoPorId ? console.log(productoPorId) : console.log(`Not found`);
   };
 
 };
@@ -49,10 +32,11 @@ const ProductManager1 = new ProductManager();
 ProductManager1.getProducts();
 
 ProductManager1.addProduct("producto prueba", "Este es un producto prueba", 200, "Sin imagen", "abc123", 25);
-ProductManager1.addProduct("producto prueba", "Este es un producto prueba", 200, "Sin imagen", "abc123", 25);
+//ProductManager1.addProduct("producto prueba", "Este es un producto prueba", 200, "Sin imagen", "ieie", 25);
+//ProductManager1.addProduct("producto prueba", "Este es un producto prueba", 200, "Sin imagen", "abc123", 25);
 
 
 ProductManager1.getProducts();
 
-ProductManager1.getProductById(1)
-ProductManager1.getProductById(2)
+//ProductManager1.getProductById(1)
+//ProductManager1.getProductById(3)
